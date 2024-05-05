7c3b70fbb68a3f8f4b3f433b452bcedc506b0498
Rami Chaar <Chaarrami22@gmail.com>
2024-05-05 03:21:45 -0700

    added ufo for folding

---
+++ .config/nvim/lua/rami/plugins/ufo.lua
@@ -0,0 +1,17 @@
+-- code folding
+return {
+    'kevinhwang91/nvim-ufo',
+    dependencies = { 'kevinhwang91/promise-async' },
+    config = function()
+        local ufo = require("ufo")
+
+        ufo.setup({
+            provider_selector = function(bufnr, filetype, buftype)
+                return { 'treesitter', 'indent' }
+            end
+        })
+        vim.keymap.set('n', '<leader>oz', ufo.openAllFolds)
+        vim.keymap.set('n', '<leader>cz', ufo.closeAllFolds)
+        vim.keymap.set("n", "<leader>z", "za")
+    end
+}
